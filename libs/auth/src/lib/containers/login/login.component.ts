8c8df44f8b5ac1c34e7a70765e82a60862d3ac4e
Jesús Alejandro Atao <jealeat96@gmail.com>
2021-05-22 11:59:56 -0500

    feat(session-3): Add login

--- libs/auth/src/lib/containers/login/login.component.ts
+++ libs/auth/src/lib/containers/login/login.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Authenticate } from '@dc/models';
 
 @Component({
   selector: 'dc-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoginComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  login(auth: Authenticate):void {
+    console.log(auth);
   }
 
 }
--- libs/data-models/src/index.ts
+++ libs/data-models/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/interfaces/user.interface';
+export * from './lib/interfaces/auth.interface';
 export * from './lib/types/common.types';
