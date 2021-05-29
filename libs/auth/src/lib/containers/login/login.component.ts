109f4e8f07e2a381c825a7775a0d4c49a44588a3
Jesús Alejandro Atao <jealeat96@gmail.com>
2021-05-29 10:37:22 -0500

    feat(session-4): single unsubscribe

--- libs/auth/src/lib/containers/login/login.component.ts
+++ libs/auth/src/lib/containers/login/login.component.ts
@@ -1,5 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
 import { Authenticate } from '@dc/models';
+import { Subscription } from 'rxjs';
+
 import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
@@ -8,18 +10,23 @@ import { AuthService } from '../../services/auth/auth.service';
   styleUrls: ['./login.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LoginComponent {
+export class LoginComponent implements OnDestroy {
+  subscription!: Subscription;
 
   constructor(private authService: AuthService) {}
 
   login(auth: Authenticate):void {
     console.log(auth);
-    this.authService.login(auth)
+    this.subscription = this.authService.login(auth)
       .subscribe(
         (response) => {
           console.log('response', response);
         }
       );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
