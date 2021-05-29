b75be86acb95bd34cf7cf20f03bc0bc12818d39d
Jesús Alejandro Atao <jealeat96@gmail.com>
2021-05-29 10:16:20 -0500

    feat(session-4): add layout library

---
+++ libs/layout/src/lib/layout.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { MaterialModule } from '@dc/material';
+
+import { LayoutComponent } from './containers/layout/layout.component';
+
+@NgModule({
+  imports: [CommonModule, RouterModule, MaterialModule],
+  declarations: [
+    LayoutComponent
+  ]
+})
+export class LayoutModule {}
