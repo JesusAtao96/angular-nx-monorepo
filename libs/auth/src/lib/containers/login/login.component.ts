2bdcd6dac70d4003268642a34ce0218ce8abd194
Jesús Alejandro Atao <jealeat96@gmail.com>
2021-05-29 10:48:01 -0500

    feat(session-4): unsubscribe takeUntil

--- libs/auth/src/lib/containers/login/login.component.ts
+++ libs/auth/src/lib/containers/login/login.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
 import { Authenticate } from '@dc/models';
-import { Subscription } from 'rxjs';
+import { Subject, Subscription } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 import { AuthService } from '../../services/auth/auth.service';
 
@@ -12,12 +13,16 @@ import { AuthService } from '../../services/auth/auth.service';
 })
 export class LoginComponent implements OnDestroy {
   subscription!: Subscription;
+  private unsubscribe!: Subject<void>;
 
   constructor(private authService: AuthService) {}
 
   login(auth: Authenticate):void {
     console.log(auth);
     this.subscription = this.authService.login(auth)
+      .pipe(
+        takeUntil(this.unsubscribe)
+      )
       .subscribe(
         (response) => {
           console.log('response', response);
@@ -26,7 +31,12 @@ export class LoginComponent implements OnDestroy {
   }
 
   ngOnDestroy() {
+    // Unsubscribe forma 1
     this.subscription.unsubscribe();
+
+    // Unsubscribe forma 2
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
   }
 
 }
